3ff9ff85915bc44ffeead829cabd27a595842a02
Palak <PDebnath@wfscorp.com>
2024-11-17 21:27:28 -0600

    CORS options added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors())
-//app.options('*', cors())
+app.options('*', cors())
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
